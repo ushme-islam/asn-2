06163cf7f07dac93d9ab1f37adcf009e89bf05fc
ushme-islam <ushmekhanom@gmail.com>
2024-11-09 09:29:46 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    //her0 slider
+    // $('.hero-slider').owlCarousel({
+    //     loop: true,
+    //     margin: 0,
+    //     items: 1,
+    //     dots: false,
+    //     navText: ['PREV', 'NEXT'],
+    //     smartSpeed: 1000,
+
+    //     // autoplayTimeout: 7000,
+    //     responsive: {
+    //         0: {
+    //             nav: false,
+    //         },
+    //         768: {
+    //             nav: true,
+    //         }
+    //     }
+    // });
+    // project slider
+
+//     $('#project-slider').owlCarousel({
+//         loop: true,
+//         nav: true,
+//         margin: 0,
+//         dots: false,
+//         smartSpeed: 1000,
+//         margin: 24,
+//         navText: ['NEXT', 'PREV'],
+//         responsive: {
+//             0: {
+//                 items: 1,
+//                 nav: false,
+//             },
+//             768: {
+//                 items: 2,
+//             },
+//             1140: {
+//                 items: 2,
+//                 center: true,
+//             }
+//         }
+//     })
+
+//     $('.owl-carousel').owlCarousel({
+//         loop: true,
+//         nav: false,
+//         dots: true,
+//         smartSpeed: 900,
+//         items: 1,
+//         margin: 10,
+//     })
+// });
